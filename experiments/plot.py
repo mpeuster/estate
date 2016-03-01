2d25ad83a9f656fe4e5bc25465c50610786240c5
Manuel Peuster <manuel@peuster.de>
2016-03-01 09:58:00 +0100

    better plot runner

--- experiments/plot.py
+++ experiments/plot.py
@@ -1,8 +1,9 @@
 from plot import plot_single_scenario
 from plot import plot_multi_scenario
+import sys
 
 
-def do_single_scenario_plots(exp_to_plot=["simple-poc-comparisson"]):
+def do_single_scenario_plots(exp_to_plot=["simple-poc-comparisson", "scale-down-hack"]):
     for e in exp_to_plot:
         print "=" * 70
         print "Plotting experiment: %s" % e
@@ -19,10 +20,16 @@ def do_multi_scenario_plots(exp_to_plot=["scaleability-fixed"]):
 
 
 def main(args=None):
-    do_single_scenario_plots()
-    do_multi_scenario_plots()
+    if len(args) < 2:
+        do_single_scenario_plots()
+        do_multi_scenario_plots()
+    else:
+        if "-s" in args:
+            do_single_scenario_plots()
+        elif "-m" in args:
+            do_multi_scenario_plots()
     print "I'am done. Be happy."
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
